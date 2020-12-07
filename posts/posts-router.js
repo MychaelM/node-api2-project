4c60e6bc9c3b3ec2f6a35cdaf7e4156d9cb32f87
Mychael M <mychaelbmenges@gmail.com>
2020-12-06 18:39:08 -0700

    added endpoint to delete a post

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -33,6 +33,7 @@ router.get(`/api/posts/:id`, (req, res) => {
       });
     })
 })
+
 router.get(`/api/posts/:id/comments`, (req, res) => {
   posts.findPostComments(req.params.id)
     .then((comments) => {
@@ -51,6 +52,7 @@ router.get(`/api/posts/:id/comments`, (req, res) => {
       });
     })
 })
+
 router.post(`/api/posts`, (req, res) => {
   if (!req.body.title || !req.body.contents) {
     return res.status(400).json({
@@ -69,6 +71,7 @@ router.post(`/api/posts`, (req, res) => {
       });
     })
 })
+
 router.post(`/api/posts/:id/comments`, (req, res) => {
   if (!req.body.text) {
     return res.status(400).json({
@@ -97,6 +100,7 @@ router.post(`/api/posts/:id/comments`, (req, res) => {
       });
     })
 })
+
 router.put(`/api/posts/:id`, (req, res) => {
   posts.findById(req.params.id)
     .then((post) => {
@@ -131,14 +135,39 @@ router.put(`/api/posts/:id`, (req, res) => {
       });
     })
 })
+
 router.delete(`/api/posts/:id`, (req, res) => {
   posts.findById(req.params.id)
     .then(() => {
-
+      posts.findById(req.params.id)
+    .then((post) => {
+      if (post[0]) {
+        posts.remove(req.params.id)
+          .then((numberOfRecords) => {
+            res.status(200).json({
+              recordsDeleted: numberOfRecords,
+              postRemoved: post
+            })
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: "The post could not be removed.",
+            });
+          })
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      }
     })
     .catch((err) => {
-
+      console.log(err);
+      res.status(500).json({
+        error: "The post could not be removed",
+      });
     })
+})
 })
 
 module.exports = router;
