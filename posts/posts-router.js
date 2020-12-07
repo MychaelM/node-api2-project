822605f5abf147114f7787fe3c75908c1b93d1b7
Mychael M <mychaelbmenges@gmail.com>
2020-12-06 18:29:12 -0700

    added endpoint to update posts

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -99,11 +99,36 @@ router.post(`/api/posts/:id/comments`, (req, res) => {
 })
 router.put(`/api/posts/:id`, (req, res) => {
   posts.findById(req.params.id)
-    .then(() => {
+    .then((post) => {
+      if (post[0]) {
+        console.log(post);
+        if (!req.body.title || !req.body.contents) {
+          return res.status(400).json({
+            errorMessage: "Please provide title and contents for the post.",
+          });
+        }
+        posts.update(req.params.id, req.body)
+          .then((updatedPost) => {
+            res.status(200).json(req.body)
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: "The post information could not be modified.",
+            });
+          })
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      }
 
     })
     .catch((err) => {
-
+      console.log(err)
+      res.status(500).json({
+        error: "The post information could not be modified.",
+      });
     })
 })
 router.delete(`/api/posts/:id`, (req, res) => {
